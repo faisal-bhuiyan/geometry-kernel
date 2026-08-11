7a804c6e0c6a822a3c0d172015a9a2c08f72c0ce
Faisal Hasan <faisalhbhuiyan@gmail.com>
2026-08-10 19:04:43 -0600

    Add 3D points to point.hpp

--- src/core/point.hpp
+++ src/core/point.hpp
@@ -34,4 +34,33 @@ template <ScalarType T>
     return (std::abs(a.x - b.x) <= kTolerance<T>) && (std::abs(a.y - b.y) <= kTolerance<T>);
 }
 
+//---------------------------------------------------------------------------
+// Point in 3D space
+//---------------------------------------------------------------------------
+
+/// @brief 3D point in space
+template <ScalarType T>
+struct Point3 {
+    T x{};
+    T y{};
+    T z{};
+};
+
+using Point3D = Point3<double>;  ///< 3D point in space with double precision
+
+/**
+ * @brief Returns true if two points are within @p kTolerance of each other in all coordinates.
+ *
+ * Used to detect duplicate vertices without exact floating-point equality.
+ *
+ * @param a First point.
+ * @param b Second point.
+ * @return True if |a.x - b.x| <= kTolerance and likewise for y, z.
+ */
+template <ScalarType T>
+[[nodiscard]] inline bool NearlyEqualPoint(const Point3<T>& a, const Point3<T>& b) {
+    return (std::abs(a.x - b.x) <= kTolerance<T>) && (std::abs(a.y - b.y) <= kTolerance<T>) &&
+           (std::abs(a.z - b.z) <= kTolerance<T>);
+}
+
 }  // namespace geometry_kernel::core
